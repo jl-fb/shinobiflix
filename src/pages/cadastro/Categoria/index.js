c9d45d88820bea48f8653b105b94793339473325
Joao Borghezan <joao@clic.digital>
2020-07-31 19:13:46 -0300

    Adiciona db.json e fetch para get categorias

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,49 +1,67 @@
-import React, {useState} from 'react'
-import PageDefault from '../../../components/PageDefault'
-import { Link } from 'react-router-dom'
-import FormField from '../../../components/FormField'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
+import BASE_URL from '../../../environments/environment';
 
 function CadastroCategoria() {
-
   const valoresIniciais = {
     nome: '',
     descricao: '',
     cor: '',
-  } 
+  };
 
-  const [categorias, setCategorias] = useState([])
-  const [categoria, setValues] = useState(valoresIniciais)
-  
-  function mudarCategoria(chave, valor){
-      setValues({
-        ...categoria, 
-        [chave]: valor, 
-      })
+  const [categorias, setCategorias] = useState([]);
+  const [categoria, setValues] = useState(valoresIniciais);
+
+  function mudarCategoria(chave, valor) {
+    setValues({
+      ...categoria,
+      [chave]: valor,
+    });
   }
-    function handleChange(infosDoEvento) {
+  function handleChange(infosDoEvento) {
     mudarCategoria(
       infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value
+      infosDoEvento.target.value,
     );
   }
-  
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/categorias`)
+      .then(async (resp) => {
+        const resposta = await resp.json();
+        console.log(resposta);
+        setCategorias([
+          ...resposta,
+        ]);
+      });
+  }, []);
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {categoria.nome}</h1>
-      <form onSubmit={function handleSubmit(e){
-        e.preventDefault()
-        setCategorias([...categorias, categoria])
-        
-        setValues(valoresIniciais)
-      }}>
-        <FormField 
+      <h1>
+        Cadastro de Categoria:
+        {categoria.nome}
+      </h1>
+
+      <form onSubmit={function handleSubmit(e) {
+        e.preventDefault();
+        setCategorias([...categorias, categoria]);
+
+        setValues(valoresIniciais);
+      }}
+
+      >
+        <FormField
           label="Nome da Categoria:"
           type="text"
           name="nome"
           value={categoria.nome}
           onChange={handleChange}
         />
-        <FormField 
+        <FormField
           label="Descrição:"
           type="textarea"
           name="descricao"
@@ -51,28 +69,30 @@ function CadastroCategoria() {
           onChange={handleChange}
         />
 
-        <FormField 
+        <FormField
           label="Cor:"
           type="color"
           name="cor"
           value={categoria.descricao}
           onChange={handleChange}
-        />  
-      
-        <button >Cadastrar</button>
+        />
 
+        <Button>Cadastrar</Button>
+        {categoria.length === 0 && (
+          <div>
+            Loading...
+          </div>
+        )}
         <ul>
-          {categorias.map((categoria, i) => {
-            return(
-            <li key={`${categoria}${i}`}>{categoria.nome}</li>
-            )
-          })}
+          {categorias.map((categoria) => (
+            <li key={`${categoria.titulo}${Math.random(Date.now() * 1)}`}>{categoria.titulo}</li>
+          ))}
         </ul>
 
       </form>
-      <Link to='/'>Ir para Home</Link>
+      <Link to="/">Ir para Home</Link>
     </PageDefault>
-  )
+  );
 }
 
-export default CadastroCategoria
+export default CadastroCategoria;
